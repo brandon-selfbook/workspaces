748cfaec001aba1dbd804ac6007eb1f7cd3d45d4
brandon-selfbook <brandon@selfbook.com>
2023-06-13 16:42:39 -0700

    feat: use private package

--- packages/ui-kit/index.js
+++ packages/ui-kit/index.js
@@ -1,7 +1,9 @@
 var delivery = require("@brandon_selfbook/delivery");
+var private = require("@brandon_selfbook/private");
 var pkg = require("./package.json");
 
 module.exports = () => {
     console.log('UI Kit ' + pkg.version);
     delivery('Fuck yeah!');
+    private();
 };
