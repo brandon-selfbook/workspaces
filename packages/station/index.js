60fbdfc8a4eef1f54615cb81140a5fdf97c54ff0
brandon-selfbook <brandon@selfbook.com>
2023-06-13 10:04:07 -0700

    chore: initial commit

---
+++ packages/station/index.js
@@ -0,0 +1,5 @@
+var uiKit = require('@selfbook/ui-kit');
+
+console.log('Station v1.0.0.');
+console.log('   Imported UI Kit:');
+uiKit();
---
+++ packages/ui-kit/index.js
@@ -0,0 +1,4 @@
+var pkg = require("./package.json");
+module.exports = () => {
+    console.log('   UI Kit ' + pkg.version);
+};
